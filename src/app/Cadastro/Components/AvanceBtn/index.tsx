bb690e2595aaf6d903e5e1edce6c168a080143d6
jvcruz <joaovictormdc99@gmail.com>
2024-08-10 15:31:49 -0300

    Inputs

--- src/app/Cadastro/Components/AvanceBtn/index.tsx
+++ src/app/Cadastro/Components/AvanceBtn/index.tsx
@@ -1,11 +1,12 @@
 interface AvanceBtnProps {
 
     AvanceFunction?: () => void,
-    ProgressStatus?: number
+    ProgressStatus?: number,
+    ProgressLogin?: number,
 }
 
 
-function AvanceBtn({ AvanceFunction, ProgressStatus }: AvanceBtnProps) {
+function AvanceBtn({ AvanceFunction, ProgressStatus, ProgressLogin }: AvanceBtnProps) {
 
     //CASO A PROGRESS BAR SEJA IGUAL A ZERO. ELE VAI RENDERIZAR O BOTAO COM O TEXTO INICIAR
     if (ProgressStatus === 0) {
@@ -15,7 +16,15 @@ function AvanceBtn({ AvanceFunction, ProgressStatus }: AvanceBtnProps) {
             <button className="Btn_Primary" onClick={AvanceFunction}>Iniciar</button>
         </>);
 
-    }else{
+    }else if(ProgressLogin === 0) {
+        return(
+            <>
+            <button className="Btn_Primary" onClick={AvanceFunction}>Entrar</button>
+            </>
+        )
+    }
+    
+    else{
 
         return (<>
 
--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,19 +1,77 @@
+"use client"
+
 import AvanceBtn from "../Cadastro/Components/AvanceBtn";
 import HeaderCadastros from "../Cadastro/Components/Header";
+import { useState } from "react";
+import { useRouter } from 'next/navigation'
+import { IoMail, IoEyeOff, IoEye, IoLockClosed } from "react-icons/io5";
 
 
-function Page (){
-    return(
-        <>
-        <main className="p-4 flex flex-col h-screen gap-5">
-        <HeaderCadastros/>
+function Page() {
 
-        <h1>Aaaa</h1>
+    const [progressBar, setProgressBar] = useState(0)
+    const router = useRouter()
 
-        <AvanceBtn/>
-        </main>
-        </>
-    )
+    function Avance() {
+
+        setProgressBar(progressBar + 1)
+    }
+
+    function Back() {
+
+        setProgressBar(progressBar - 1)
+
+    }
+
+    function BackToHome() {
+
+        if (progressBar == 0) {
+
+            router.push("/")
+
+        }
+
+    }
+
+
+    switch (progressBar) {
+        case 0:
+            return (
+                <>
+                    <main className="p-4 flex flex-col h-screen gap-5">
+                        <HeaderCadastros BackFunction={BackToHome} />
+
+                            <article className="flex flex-col gap-2 mb-10 mt-10">
+                                <span className="text-xl font-extrabold text-zinc-700">Bem-vindo(a) de volta!</span>
+                                <span className="text-sm font-medium text-zinc-500">Preencha seus dados cadastrados</span>
+                            </article>
+
+                            <div className="flex flex-col gap-2 mb-6">
+                                <label className="labelDef">Seu Email</label>
+                                <input type="email" placeholder="Digite seu email..." className="inputDef"/>
+                            </div>
+
+                            <div className="flex flex-col gap-2 mb-6">
+                                <label className="labelDef">Senha</label>
+                                <input type="password" placeholder="Digite sua senha..." className="inputDef"/>
+                            </div>
+
+                        <AvanceBtn AvanceFunction={Avance} ProgressLogin={progressBar} />
+                    </main>
+                </>
+            )
+
+        case 1:
+            return (
+                <>
+                 <main className="p-4 flex flex-col justify-between h-screen gap-5">
+                 <HeaderCadastros BackFunction={Back} />
+
+                 <AvanceBtn AvanceFunction={Avance} ProgressLogin={progressBar} />
+                 </main>
+                </>
+            )
+    }
 }
 
 export default Page;
