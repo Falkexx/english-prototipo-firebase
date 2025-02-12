1db06a2aa8078c77463ec4ff5cbf31db7ebb43ce
AndreSilvazm <andre.mani2015@gmail.com>
2025-02-12 12:45:24 -0300

    :phone: respo : Adicionado responsividade a tela de Rankin

--- src/app/[locale]/RankingPage/page.tsx
+++ src/app/[locale]/RankingPage/page.tsx
@@ -1,25 +1,28 @@
-import PeriodSection from './Components/PeriodSecion'
-import UserRanking from './Components/UsersRanking'
+import PeriodSection from "./Components/PeriodSecion";
+import UserRanking from "./Components/UsersRanking";
 import BottomHeader from "../Home/Components/BottomHeader";
 import IsAuthenticated from "@/services/IsAuthenticaded";
+import Sidebar from "../Home/Components/Desktop/Sidebar/Sidebar";
 
 async function page() {
+  await IsAuthenticated();
 
-    await IsAuthenticated();
-    
-    return (
+  return (
+    <>
+      <section className="lg:flex lg:flex-row lg:w-full lg:m-auto lg:gap-8">
+        <div className="hidden lg:block lg:w-[23%]">
+          <Sidebar ActualPath="RankingPage" />
+        </div>
 
-        <>
-            <BottomHeader ActualPath="RankingPage"/>
+        <BottomHeader ActualPath="RankingPage" />
 
-            <main className="px-6">
-
-                <PeriodSection/>
-                <UserRanking/>
-
-            </main>
-        </>
-    );
+        <main className="px-6 lg:px-6 lg:w-[80%] lg:h-[50vh] lg:mt-10">
+          <PeriodSection />
+          <UserRanking />
+        </main>
+      </section>
+    </>
+  );
 }
 
 export default page;
